c441f7afb49e3f6d3976b6a76b8849a2b11c8367
Akshay Meher <akshay_111meher@hotmail.com>
2017-02-10 15:51:03 +0530

    inserting employees completed

--- ex2/ex2.go
+++ ex2/ex2.go
@@ -3,12 +3,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"strconv"
+	"encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
+}
+
+
+type Employee struct {
+	name string 
+	employeeId int
+	project string
 }
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
@@ -18,11 +28,64 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-    function, _ := stub.GetFunctionAndParameters()
+    function, args := stub.GetFunctionAndParameters()
     fmt.Println("Invoke is running " + function)
-	return shim.Success(nil)
+    
+    if function =="initEmployee"{
+		return t.initEmployee(stub,args)
+	}
+	return shim.Error("Received unknown function invocation")
 }
 
+func (t *SimpleChaincode) initEmployee(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+	
+	if len(args) != 3 {
+		return shim.Error("Incorrect number of arguments. Expecting 3")
+	}
+	// ==== Input sanitation ====
+	fmt.Println("- start initEmployee")
+	if len(args[0]) <= 0 {
+		return shim.Error("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return shim.Error("2nd argument must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return shim.Error("3rd argument must be a non-empty string")
+	}
+	
+	employeeName := args[0]
+	project := strings.ToLower(args[2])
+	employeeId, err := strconv.Atoi(args[1])
+	employeeIdAsString := args[1]
+	if err != nil {
+		return shim.Error("2rd argument must be a numeric string")
+	}
+	
+	employeeAsBytes, err := stub.GetState(employeeIdAsString)
+	if err != nil {
+		return shim.Error("Failed to get employee: " + err.Error())
+	} else if employeeAsBytes != nil {
+		fmt.Println("This employee already exists: " + employeeIdAsString)
+		return shim.Error("This marble already exists: " + employeeIdAsString)
+	}
+	
+	employee:= &Employee{employeeName,employeeId,project}
+	
+	employeeJSONasBytes, err := json.Marshal(employee)
+	
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = stub.PutState(employeeIdAsString, employeeJSONasBytes)
+	
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	
+	fmt.Println("- end initEmployee")
+	return shim.Success(nil)
+}
 
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface) pb.Response {
     function, _ := stub.GetFunctionAndParameters()
