bb5ca0e37535a790e1daf85951c2ccc46e5ae67c
Akshay Meher <akshay_111meher@hotmail.com>
2017-02-17 11:12:03 +0530

    version 0.6 chaincode added

--- ex2versionpoint6/ex2versionpoint6.go
+++ ex2versionpoint6/ex2versionpoint6.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 	"strings"
 	"strconv"
+	"errors"
 	"encoding/json"
-	"encoding/base64"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	//pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // SimpleChaincode example simple Chaincode implementation
@@ -32,14 +31,13 @@ type Project struct {
 	customerOf string
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	function, _ := stub.GetFunctionAndParameters()
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte,error) {
     fmt.Println("Init is running " + function)
-    return shim.Success(nil)
+    return nil,nil
 }
 
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-    function, args := stub.GetFunctionAndParameters()
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte,error) {
+    
     fmt.Println("Invoke is running " + function)
     
     if function =="initEmployee"{
@@ -55,24 +53,24 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	}else if function =="getProject"{
 		return t.getProject(stub,args)
 	}
-	return shim.Error("Received unknown function invocation")
+	return nil,errors.New("Received unknown function invocation")
 }
 
-func (t *SimpleChaincode) initProject(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+func (t *SimpleChaincode) initProject(stub shim.ChaincodeStubInterface, args []string) ([]byte,error){
 	
 	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
+		return nil,errors.New("Incorrect number of arguments. Expecting 3")
 	}
 	// ==== Input sanitation ====
 	fmt.Println("- start initProject")
 	if len(args[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string")
+		return nil,errors.New("1st argument must be a non-empty string")
 	}
 	if len(args[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string")
+		return nil,errors.New("2nd argument must be a non-empty string")
 	}
 	if len(args[2]) <= 0 {
-		return shim.Error("3rd argument must be a non-empty string")
+		return nil,errors.New("3rd argument must be a non-empty string")
 	}
 	
 	projectName := args[0]
@@ -80,15 +78,15 @@ func (t *SimpleChaincode) initProject(stub shim.ChaincodeStubInterface, args []s
 	projectId, err := strconv.Atoi(args[1])
 	projectIdAsString := args[1]
 	if err != nil {
-		return shim.Error("2rd argument must be a numeric string")
+		return nil,errors.New("2nd argument must be a numeric string")
 	}
 	
 	projectAsBytes, err := stub.GetState(projectIdAsString)
 	if err != nil {
-		return shim.Error("Failed to get project: " + err.Error())
+		return nil,err
 	} else if projectAsBytes != nil {
 		fmt.Println("This project already exists: " + projectIdAsString)
-		return shim.Error("This project already exists: " + projectIdAsString)
+		return nil, errors.New("This project already exists "+projectIdAsString)
 	}
 	
 	project:= &Project{projectName,projectId,customerOf}
@@ -96,80 +94,81 @@ func (t *SimpleChaincode) initProject(stub shim.ChaincodeStubInterface, args []s
 	projectJSONasBytes, err := json.Marshal(project)
 	
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil,err
 	}
 	err = stub.PutState(projectIdAsString, projectJSONasBytes)
 	
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil,err
 	}
 	
 	// composite key creation for searching projects according to customer
+	/*
 	indexName := "customerOf"
 	customerOfIndexKey, err := stub.CreateCompositeKey(indexName, []string{customerOf, projectIdAsString})
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil,err
 	}
 
 	value := []byte{0x00}
 	stub.PutState(customerOfIndexKey, value)
-
+    */
 	fmt.Println("- end initProject")
-	return shim.Success(nil)
+	return nil,nil
 }
 
-func (t *SimpleChaincode) getProject(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+func (t *SimpleChaincode) getProject(stub shim.ChaincodeStubInterface, args []string) ([]byte,error){
 	if len(args) !=1{
-		return shim.Error("Incorrect number of arguments. Expecting 1")
+		return nil,errors.New("Incorrect number of arguments. Expecting 1")
 	}
 	projectId := args[0]
 	project, err := stub.GetState(projectId)
 	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed retrieving employee [%s]: [%s]", projectId, err))
+		return nil,err
 	}
 	
-	return shim.Success([]byte(base64.StdEncoding.EncodeToString(project)))
+	return project,nil
 }
 
 
-func (t *SimpleChaincode) getCustomer(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+func (t *SimpleChaincode) getCustomer(stub shim.ChaincodeStubInterface, args []string) ([]byte,error){
 	if len(args) !=1{
-		return shim.Error("Incorrect number of arguments. Expecting 1")
+		return nil,errors.New("Incorrect number of arguments. Expecting 1")
 	}
 	customerId := args[0]
 	customer, err := stub.GetState(customerId)
 	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed retrieving employee [%s]: [%s]", customerId, err))
+		return nil,err
 	}
 	
-	return shim.Success([]byte(base64.StdEncoding.EncodeToString(customer)))
+	return customer,nil
 }
 
-func (t *SimpleChaincode) initCustomer(stub shim.ChaincodeStubInterface,args []string)pb.Response{
+func (t *SimpleChaincode) initCustomer(stub shim.ChaincodeStubInterface,args []string) ([]byte,error) {
 		if len(args) != 2{
-			return shim.Error("Incorrect number of arguments. Expecting 2")
+			return nil,errors.New("Incorrect number of arguments. Expecting 2")
 		}	
 			// ==== Input sanitation ====
 		fmt.Println("- start initCustomer")
 		if len(args[0]) <= 0 {
-			return shim.Error("1st argument must be a non-empty string")
+			return nil,errors.New("1st argument must be a non-empty string")
 		}
 		if len(args[1]) <= 0 {
-			return shim.Error("2nd argument must be a non-empty string")
+			return nil,errors.New("2nd argument must be a non-empty string")
 		}
 		
 		customerName := args[0]
 		customerId, err := strconv.Atoi(args[1])
 		customerIdAsString := args[1]
 		if err != nil {
-			return shim.Error("2rd argument must be a numeric string")
+			return nil,errors.New("2nd argument must be a numeric string")
 		}
 		customerAsBytes, err := stub.GetState(customerIdAsString)
 		if err != nil {
-			return shim.Error("Failed to get customer: " + err.Error())
+			 return nil,err
 		} else if customerAsBytes != nil {
 			fmt.Println("This customer already exists: " + customerIdAsString)
-			return shim.Error("This customer already exists: " + customerIdAsString)
+			return nil,errors.New("This customer already exists: "+customerIdAsString)
 		}
 
 		customer:= &Customer{customerName,customerId}
@@ -177,33 +176,33 @@ func (t *SimpleChaincode) initCustomer(stub shim.ChaincodeStubInterface,args []s
 		customerJSONasBytes, err := json.Marshal(customer)
 		
 		if err != nil {
-			return shim.Error(err.Error())
+			return nil,err
 		}
 		err = stub.PutState(customerIdAsString, customerJSONasBytes)
 		
 		if err != nil {
-			return shim.Error(err.Error())
+			return nil,err
 		}
 		
 		fmt.Println("- end initCustomer")
-		return shim.Success(nil)
+		return nil,nil
 }
 
-func (t *SimpleChaincode) initEmployee(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+func (t *SimpleChaincode) initEmployee(stub shim.ChaincodeStubInterface, args []string) ([]byte,error){
 	
 	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
+		return nil,errors.New("Incorrect number of arguments. Expecting 3")
 	}
 	// ==== Input sanitation ====
 	fmt.Println("- start initEmployee")
 	if len(args[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string")
+		return nil,errors.New("1st argument must be a non-empty string")
 	}
 	if len(args[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string")
+		return nil,errors.New("2nd argument must be a non-empty string")
 	}
 	if len(args[2]) <= 0 {
-		return shim.Error("3rd argument must be a non-empty string")
+		return nil,errors.New("3rd argument must be a non-empty string")
 	}
 	
 	employeeName := args[0]
@@ -211,15 +210,15 @@ func (t *SimpleChaincode) initEmployee(stub shim.ChaincodeStubInterface, args []
 	employeeId, err := strconv.Atoi(args[1])
 	employeeIdAsString := args[1]
 	if err != nil {
-		return shim.Error("2rd argument must be a numeric string")
+		return nil,errors.New("2nd argument must be a numeric string")
 	}
 	
 	employeeAsBytes, err := stub.GetState(employeeIdAsString)
 	if err != nil {
-		return shim.Error("Failed to get employee: " + err.Error())
+		return nil,err
 	} else if employeeAsBytes != nil {
 		fmt.Println("This employee already exists: " + employeeIdAsString)
-		return shim.Error("This employee already exists: " + employeeIdAsString)
+		return nil,errors.New("This employee already exists: "+employeeIdAsString)
 	}
 	
 	employee:= &Employee{employeeName,employeeId,project}
@@ -227,49 +226,55 @@ func (t *SimpleChaincode) initEmployee(stub shim.ChaincodeStubInterface, args []
 	employeeJSONasBytes, err := json.Marshal(employee)
 	
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil,err
 	}
 	err = stub.PutState(employeeIdAsString, employeeJSONasBytes)
 	
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil,err
 	}
 	
 	// composite key to get employees by project
+	
+	/* 
 	indexName := "project"
 	projectIndexKey, err := stub.CreateCompositeKey(indexName, []string{project, employeeIdAsString})
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil,err
 	}
 
 	value := []byte{0x00}
-	stub.PutState(projectIndexKey, value)
+	stub.PutState(projectIndexKey, value) 
+	*/
 	fmt.Println("- end initEmployee")
-	return shim.Success(nil)
+	return nil,nil
 }
 
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface) pb.Response {
-    function, args := stub.GetFunctionAndParameters()
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte,error) {
     fmt.Println("Query is running " + function)
     
     if function =="getEmployee"{
 		return t.getEmployee(stub,args)
+	}else if function =="getCustomer"{
+		return t.getCustomer(stub,args)
+	}else if function =="getProject"{
+		return t.getProject(stub,args)
 	}
 	
-	return shim.Error("Received unknown function query")
+	return nil,errors.New("Received unknown function query")
 }
 
-func (t *SimpleChaincode) getEmployee(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+func (t *SimpleChaincode) getEmployee(stub shim.ChaincodeStubInterface, args []string) ([]byte,error){
 	if len(args) !=1{
-		return shim.Error("Incorrect number of arguments. Expecting 1")
+		return nil,errors.New("Incorrect number of arguments. Expecting 1")
 	}
 	employeeId := args[0]
 	employee, err := stub.GetState(employeeId)
 	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed retrieving employee [%s]: [%s]", employeeId, err))
+		return nil,err
 	}
 	
-	return shim.Success([]byte(base64.StdEncoding.EncodeToString(employee)))
+	return employee,nil
 }
 
 
